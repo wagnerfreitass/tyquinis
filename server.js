ac064697ca61f7d1e647dad4fda4dc45be5535fa
Rafael Silveira de Souza <rafael.silveira@ed.dev.br>
2025-02-27 18:29:41 -0300

    atualização de estrutura de paginas

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Servir arquivos estáticos da pasta atual (ou ajuste para a pasta do seu projeto)
+app.use(express.static(__dirname));
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
