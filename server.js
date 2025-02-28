c1b5773d38ea0cd32ced59256317b7474f79e785
Wagner Freitas <waguinho.freitas55@hotmail.com>
2025-02-28 14:13:36 -0300

    Atualização de algumas categorias

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 // Servir arquivos estáticos da pasta atual (ou ajuste para a pasta do seu projeto)
 app.use(express.static(__dirname));
 
-// Inicia o servidor
+// Inicia o servidor "node server.js"
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
-});
+});"'"
